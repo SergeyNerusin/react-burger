e5d737997bfb29222c155320e3b84515bebf0a4c
Nerusin Sergey <s.nerusin@yandex.ru>
2022-09-11 18:50:35 +0300

    BurgerConstructor all

--- src/components/BurgerConstructor/BurgerConstructor.jsx
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -5,8 +5,8 @@ import PropTypes from 'prop-types';
 
 function BurgerConstructor({data}) {
   return (
-    <article className={style.container + ' pl-4'}>
-       <div className={style.itemlock  + ' pr-4 mt-25 mb-4'}>
+    <article className={style.container + ' ml-4'}>
+       <div className={style.itemlock  + ' mr-4 mt-25 mb-4'}>
         <ConstructorElement
           type="top"
           isLocked={true}
@@ -15,7 +15,23 @@ function BurgerConstructor({data}) {
           thumbnail={data[0].image}
         />
       </div>
-      <div className={style.itemlock + ' pr-4'}>
+      <div>
+        <ul className={style.fillings + ' ml-4'}>
+          {data.filter((filling) => filling.type !== 'bun').map((filling) => (
+            <li key={filling._id} className={style.item}>
+              <span className='mr-2'>
+                <DragIcon  type="primary" />
+              </span>
+              <ConstructorElement
+                text={filling.name}
+                price={filling.price}
+                thumbnail={filling.image}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className={style.itemlock + ' mr-4'}>
         <ConstructorElement
           type="bottom"
           isLocked={true}
@@ -24,7 +40,7 @@ function BurgerConstructor({data}) {
           thumbnail={data[0].image}
         />
       </div>
-      <div className={style.order + ' pr-4 mt-10'}>
+      <div className={style.order + ' mr-4 mt-10'}>
         <div className={style.total + ' mr-10'}>
           <p className="text text_type_digits-medium mr-2">610</p> 
           <CurrencyIcon type="primary" /> 
