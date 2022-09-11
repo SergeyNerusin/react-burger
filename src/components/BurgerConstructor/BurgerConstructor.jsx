25afe7bfd88aee13956c4382eca4da4cf0db42ab
Nerusin Sergey <s.nerusin@yandex.ru>
2022-09-11 17:06:12 +0300

    BurgerConstructor -(низ) + button make order

--- src/components/BurgerConstructor/BurgerConstructor.jsx
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -15,6 +15,24 @@ function BurgerConstructor({data}) {
           thumbnail={data[0].image}
         />
       </div>
+      <div className={style.itemlock + ' pr-4'}>
+        <ConstructorElement
+          type="bottom"
+          isLocked={true}
+          text={data[0].name + ' (низ)'}
+          price={data[0].price}
+          thumbnail={data[0].image}
+        />
+      </div>
+      <div className={style.order + ' pr-4 mt-10'}>
+        <div className={style.total + ' mr-10'}>
+          <p className="text text_type_digits-medium mr-2">610</p> 
+          <CurrencyIcon type="primary" /> 
+        </div>
+          <Button type="primary" size="large">
+            Оформить заказ
+         </Button>
+      </div>
      </article>
   )
 }
