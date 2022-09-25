d017516947b39957a377f569592c2d189cce0642
Nerusin Sergey <s.nerusin@yandex.ru>
2022-09-25 14:08:23 +0300

    recovery ingredient.jsx

---
+++ src/components/burger-ingredients/ingredient/ingredient.jsx
@@ -0,0 +1,27 @@
+import {CurrencyIcon, Counter} from '@ya.praktikum/react-developer-burger-ui-components';
+import React from 'react';
+import PropTypes from 'prop-types';
+import style from './ingredient.module.css';
+
+function Ingredient({data, openModal}) {
+  const [count, setCount] = React.useState(0);
+  
+  return (
+    <li className={style.ingredient} onClick={() => openModal(data)}>
+      <Counter count={count} size="default" onClick={setCount}/>
+      <img src={data.image} alt={data.name} className={style.image + ' pr-4 pl-4'}/>
+      <div className={style.price + ' mt-1 pr-4 pl-4'}>
+        <p className='text text_type_digits-default'>{data.price}</p>
+        <CurrencyIcon type="primary" />
+      </div>
+      <p className="text text_type_main-default mt-1">{data.name}</p>
+    </li>
+  )
+}
+
+Ingredient.propTypes = {
+  data: PropTypes.object.isRequired,
+  openModal: PropTypes.func.isRequired
+};
+
+export default Ingredient
