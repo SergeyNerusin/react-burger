4df71160560615afae4f70a52ba5fa00fd3ec55b
Nerusin Sergey <s.nerusin@yandex.ru>
2022-12-29 00:26:33 +0300

    modified api, stats component and styles

--- src/components/stats/stats.jsx
+++ src/components/stats/stats.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import styles from './stats.module.css';
+import PropTypes from 'prop-types';
+import { BUN_ID } from "../../utils/constant";
 
 export const Stats = ({ data }) =>{
 
+  /* 
+      Сортировка номеров заказов по статусу, с проверкой 
+      на неполные заказы (отсутствие булок или ингредиентов)
+      по аналогии с функцией  useBurgerIngredients 
+      в катологе: src/utils/use-burger-ingredients.js
+   */
   const sorting = (name) => 
-    data.orders.filter(order =>  order.status === `${name}`);
+    data.orders.filter(order => {
+      if (order.status === `${name}`){
+         const burg_id = order.ingredients.reduce((prevVal, item) => (
+        // eslint-disable-next-line
+        prevVal[item] = (prevVal[item] || 0) + 1, prevVal
+     ),{});
+     let burgIngr = Object.keys(burg_id);
+     if((burgIngr.length < 2) || (!burgIngr.some(ingr => BUN_ID.includes(ingr)))){
+       return null;
+     } else {
+      return order;
+     }
+    }
+    return null; 
+    });
 
   const done = sorting('done');
   const pending = sorting('pending');  
@@ -16,9 +38,11 @@ return(
           <h2 className='text text_type_main-medium pb-6'>Готовы:</h2>
           <ul className={styles.items_stats}>
             { 
-              done.map((order, index) => (index < 10)? 
-              <li key={order._id} className={styles.item_stats + ' text text_type_digits-default'}>
-                {order.number}
+              done.map(({number}, index) => (index < 20)? 
+              <li key={number} className={styles.item_stats}>
+                <span className={styles.item_text + ' text text_type_digits-default'}>
+                  {number}
+                </span>
               </li> : null)
             }
           </ul>
@@ -27,9 +51,9 @@ return(
           <h2 className='text text_type_main-medium pb-6'>В&nbsp;работе:</h2>
           <ul className={styles.items_stats}>
             {
-              pending.map((order, index) => (index < 10)? 
-              <li key={order._id} className={styles.item_stats + ' text text_type_digits-default'}>
-                {order.number}
+              pending.map(({number}, index) => (index < 20)? 
+              <li key={number} className={styles.item_stats + ' text text_type_digits-default'}>
+                {number}
               </li> : null)
             }
           </ul>
@@ -47,4 +71,8 @@ return(
     </section>
   </>
 );
+};
+
+Stats.propTypes = {
+	data: PropTypes.object.isRequired
 };
